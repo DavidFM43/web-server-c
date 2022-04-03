a331c0ba3804ffeb2cc7cd0396e9bc0c908a7449
JJFigpad <66284210+JJFigpad@users.noreply.github.com>
2022-04-03 17:36:24 -0500

    Add files via upload

---
+++ practica1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("%s", "Bienvenido\n1. Ingresar origen\n2. Ingresar destino\n");
+	printf("%s", "3. Ingresar hora\n4. Buscar tiempo de viaje medio\n");
+	printf("%s", "5. Salir\n");
+
+}
