deb9fa571bb9c22ae4fc04da73627a4133597dde
JJFigpad <66284210+JJFigpad@users.noreply.github.com>
2022-04-11 17:59:33 -0500

    Correct inputs (No de 4.)

--- P1/p1-odProgram.c
+++ P1/p1-odProgram.c
@@ -1,89 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
+void Menu();
 
 int main(){
 	int choice = 0;
-	bool running = true;
+	int c;
 
     int origin_id;
     int destination_id;
     int hour;
     float avg_travel_time;
 
-	printf("%s", "Bienvenido\n\n");
+	printf("%s", "Bienvenido\n");
 
-	while(running)
+	while(true)
 	{
-
-		printf("%s", "Por favor seleccione una de las siguientes opciones:\n");
-		printf("%s", "1. Ingresar origen\n2. Ingresar destino\n");
-		printf("%s", "3. Ingresar hora\n4. Buscar tiempo de viaje medio\n");
-		printf("%s", "5. Salir\n\n");
-
+        Menu();
 		// TODO: Account for wrong input type(all scanfs)
-		scanf("%d", &choice);
-
+        while (scanf("%d", &choice) != 1) {
+            while (!isspace(c = getchar()));
+            ungetc(c, stdin);
+            printf("Entre una opcion adecuada.\n\n");
+            Menu();
+            continue;
+        }
 		if(choice == 1){
-
-            while(true)
-			{
+            while(true){
 				printf("Ingrese ID del origen:");
-				scanf("%d", &origin_id);
-
+                while (scanf("%d", &origin_id) != 1) {
+                    while (!isspace(c = getchar()));
+                    ungetc(c, stdin);
+                    printf("Seleccione un ID entre 1 y 1160.\n");
+                    continue;
+                }
 				if(origin_id >= 1 && origin_id <= 1160){
 					// search csv file
 					break;
 				}
-				printf("Seleccione un ID entre 1 y 1160. ");
+				printf("Seleccione un ID entre 1 y 1160.\n");
 			}
-			
 		} else if(choice == 2){
-
-			while(true)
-			{
+			while(true){
 				printf("Ingrese ID del destino:");
-				scanf("%d", &destination_id);
-
+                while (scanf("%d", &destination_id) != 1) {
+                    while (!isspace(c = getchar()));
+                    ungetc(c, stdin);
+                    printf("Seleccione un ID entre 1 y 1160.\n");
+                    continue;
+                }
 				if(destination_id >= 1 && destination_id <= 1160){
 					// search csv file
 					break;
 				}
-
 				printf("Seleccione un ID entre 1 y 1160. ");
 			}
-
 		} else if(choice == 3){
 
 			while(true)
 			{
 				printf("Ingrese hora del día:");
-				scanf("%d", &hour);
-
+                while (scanf("%d", &hour) != 1) {
+                    while (!isspace(c = getchar()));
+                    ungetc(c, stdin);
+                    printf("Seleccione una hora entre 0 y 23.\n");
+                    continue;
+                }
 				if(hour >= 0 && hour <= 23){
 					// search csv file
 					break;
 				}
-
-				printf("Seleccione una hora entre 0 y 23. ");
+				printf("Seleccione una hora entre 0 y 23.\n");
 			}
 
 		} else if(choice == 4){
-
-			printf("Tiempo de viaje medio:");
-			scanf("%d", &avg_travel_time);
+            float d;
+			printf("Tiempo de viaje medio: ");
+			scanf("%f", &avg_travel_time);
 			// search csv file
-
 		} else if(choice == 5) {
 
             exit(1);
 
 		} else {
 
-            printf("Entre una opcion adecuada.\n\n");
+            printf("Entre una opcion adecuada.\n");
 
 		}
 
 	}
 }
+
+void Menu(){
+    printf("%s", "\nPor favor seleccione una de las siguientes opciones:\n");
+    printf("%s", "1. Ingresar origen\n2. Ingresar destino\n");
+    printf("%s", "3. Ingresar hora\n4. Buscar tiempo de viaje medio\n");
+    printf("%s", "5. Salir\n\n");
+}
+
