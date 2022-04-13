a90dffe273c7bd75547365945c0e1a48953fa884
JJFigpad <66284210+JJFigpad@users.noreply.github.com>
2022-04-13 18:43:21 -0500

    Update ride.h (#6)
    Round float to useful section.

--- P1/ride.h
+++ P1/ride.h
@@ -19,7 +19,7 @@ void print_ride(struct Ride ride)
     printf("Id_source: %d\n", ride.id_source);
     printf("Id_dest: %d\n", ride.id_dest);
     printf("Hour: %d\n", ride.hour);
-    printf("Average time : %f\n", ride.avg_time);
+    printf("Average time : %0.2f\n", ride.avg_time);
 }
 
 // #endif
