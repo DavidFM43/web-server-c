24e155ae3ee2b5b9a282ff1d8a377a6f2bd38bf2
= <=>
2022-04-15 23:40:34 -0500

    Pipes menu completo-no terminado :v

--- P1/menu.c
+++ P1/menu.c
@@ -1,92 +1,135 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
 void Menu();
 
 int main()
 {
-    char choice;
+	int choice = 0;
 	int c;
-	bool flag = true;
 
 	int origin_id;
 	int destination_id;
 	int hour;
 	float avg_travel_time;
 
+	int arrSend[3];
+	int fd;
+
+	if(mkfifo("myfifo", 0777) == -1){  //creating fifo file 
+		if(errno != EEXIST) {
+		 	printf("Could not create fifo file\n");
+			return 1;
+		}
+	}
+
+
 	printf("%s", "Bienvenido\n");
 
-	while (flag) {
-        Menu();
-        char s[100];
-		char buf[128];
-        char choice;
-        char *p;
-        while (fgets(buf, sizeof buf, stdin)) {
-            choice = buf[0];
-            if (choice == '1' || choice == '2' || choice == '3' || choice == '4' || choice == '5') {
-                if (buf[1] == '\n') break;
-                else printf("Introduzca una entrada adecuada.\n");
-            }
-        }
-        switch(choice){
-            case '1':
-                while (true){
-                    printf("Ingrese ID del origen: ");
-                    while (fgets(s, sizeof(s), stdin)) {
-                        origin_id = strtol(s, &p, 10);
-                        if (p == s || *p != '\n') {
-                            printf("Inserte una entrada adecuada.\nIngrese ID del origen: ");
-                        } else break;
-                    }
-                    if (origin_id >= 1 && origin_id <= 1160){
-                        // search csv file
-                        break;
-                    }
-                    printf("Seleccione un ID entre 1 y 1160.\n");
-                }
-                break;
-            case '2':
-                while (true){
-                    printf("Ingrese ID de destino: ");
-                    while (fgets(s, sizeof(s), stdin)) {
-                        destination_id = strtol(s, &p, 10);
-                        if (p == s || *p != '\n') {
-                            printf("Inserte una entrada adecuada.\nIngrese ID de destino: ");
-                        } else break;
-                    }
-                    if (destination_id >= 1 && destination_id <= 1160){
-                        // search csv file
-                        break;
-                    }
-                    printf("Seleccione un ID entre 1 y 1160.\n");
-                }
-                break;
-            case '3':
-                while (true){
-                    printf("Ingrese hora del dia: ");
-                    while (fgets(s, sizeof(s), stdin)) {
-                        hour = strtol(s, &p, 10);
-                        if (p == s || *p != '\n') {
-                            printf("Inserte una entrada adecuada.\nIngrese hora del dia: ");
-                        } else break;
-                    }
-                    if (hour >= 1 && hour <= 23){
-                        // search csv file
-                        break;
-                    }
-                    printf("Seleccione una hroa entre 1 y 23.\n");
-                }
-                break;
-            case '4': printf("Solve\n");
-                break;
-            case '5':
-                flag = false;
-                break;
-        }
+	while (true)
+	{
+		Menu();
+
+		fd = open("myfifo", O_WRONLY);
+		if (fd == -1){ //depuration
+		return 1;
+
+		while (scanf("%d", &choice) != 1) // handling of  wrong input type
+		{
+			while (!isspace(c = getchar()));
+			ungetc(c, stdin);
+			printf("Seleccione una opción adecuada.\n\n");
+			Menu();
+			continue;
+		}
+
+		if (choice == 1)
+		{
+			while (true)
+			{
+				printf("Ingrese ID del origen:");
+				while (scanf("%d", &origin_id) != 1) // handling of  wrong input type
+				{
+					while (!isspace(c = getchar()));
+					ungetc(c, stdin);
+					printf("Seleccione un ID entre 1 y 1160.\n");
+					continue;
+				}
+				if (origin_id >= 1 && origin_id <= 1160)
+				{
+					arrSend[0] = origin_id; //saving origin_id for then send
+					break;
+				}
+				printf("Seleccione un ID entre 1 y 1160.\n");
+			}
+		}
+		else if (choice == 2)
+		{
+			while (true)
+			{
+				printf("Ingrese ID del destino:");
+				while (scanf("%d", &destination_id) != 1) // handling of  wrong input type
+				{
+					while (!isspace(c = getchar()));
+					ungetc(c, stdin);
+					printf("Seleccione un ID entre 1 y 1160.\n");
+					continue;
+				}
+				if (destination_id >= 1 && destination_id <= 1160)
+				{
+					arrSend[1] = destination_id; //saving destination_id for then send
+					break;
+				}
+				printf("Seleccione un ID entre 1 y 1160. ");
+			}
+		}
+		else if (choice == 3)
+		{
+			while (true)
+			{
+				printf("Ingrese hora del día:");
+				while (scanf("%d", &hour) != 1) // handling of  wrong input type
+				{
+					while (!isspace(c = getchar()));
+					ungetc(c, stdin);
+					printf("Seleccione una hora entre 0 y 23.\n");
+					continue;
+				}
+				if (hour >= 0 && hour <= 23)
+				{
+					arrSend[2] = hour; //saving hour for then send
+					break;
+				}
+				printf("Seleccione una hora entre 0 y 23.\n");
+			}
+		}
+		else if (choice == 4)
+		{	
+			if (write(fd, arrSend, sizeof(arrSend)) == -1){ //writting in the FIFO
+				return 2; //depuration
+			}
+			float d;
+			printf("Tiempo de viaje medio: ");
+			scanf("%f", &avg_travel_time);
+			close(fd);
+		}
+		else if (choice == 5)
+		{
+			exit(1);
+			close(fd); 
+		}
+		else
+		{
+			printf("Entre una opcion adecuada.\n");
+		}
 	}
-    return 0;
 }
 
 void Menu()
