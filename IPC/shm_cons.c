6b6bf1eb98833f871592c9d962f70389b336482b
david <dmorara@unal.edu.co>
2022-05-23 17:08:21 -0500

    Procesos de shm listos
    Falta automatizar los experimentos con el script de bash

--- IPC/shm_cons.c
+++ IPC/shm_cons.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/shm.h>
+#include <string.h>
 
 #define MEM_SIZE 1024 * 1240000
 
 int main()
 {
-    key_t key = ftok("shmfile", 65);
+    key_t key = ftok("shm", 65);
   
     int shmid = shmget(key, 1024, 0666|IPC_CREAT);
   
-    char *str = (char *) shmat(shmid, 0, 0);
+    if (shmid < 0 ){
+        printf("Error in shmget.\n");
+        exit(-1);
+    }
+
+    char* str = (char*) shmat(shmid, 0, 0);
   
+    if (str < 0 ){
+        printf("Error in shmat.\n");
+        exit(-1);
+    }
+
     printf("Data read from memory: %s\n", str);
       
     shmdt(str);
@@ -18,3 +30,4 @@ int main()
     shmctl(shmid, IPC_RMID, NULL);
      
 }
+
--- IPC/shm_prod.c
+++ IPC/shm_prod.c
@@ -5,9 +5,11 @@
 
 #define MEM_SIZE 1024 * 1240000
 
+char* create_data(int);
+
 int main()
 {
-    key_t key = ftok("shmfile", 65);
+    key_t key = ftok("shm", 65);
   
     int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
 
@@ -16,14 +18,32 @@ int main()
         exit(-1);
     }
 
-    char *ptr = (char *) shmat(shmid, 0, 0);
+    char* str = (char *) shmat(shmid, 0, 0);
     
-    if (ptr < 0 ){
+    if (str < 0 ){
         printf("Error in shmat.\n");
         exit(-1);
     }
 
-    strcpy(ptr, "holamundo");
+    char* data = create_data(1);
 
-    printf("Se escribio %s\n", ptr);
+    strcpy(str, data);
+
+    printf("Data written in memory: %s\n", str);
+
+    free(data);
+}
+
+char* create_data(int kb){
+
+    char *data;
+    int bytes = 1024*kb + 1;
+    data = (char*) malloc(bytes);
+    for (int i = 0; i < bytes - 1; i++) 
+    {
+        data[i] = 'a';
+    }
+    data[bytes - 1] = '\0';
+
+    return data;
 }
